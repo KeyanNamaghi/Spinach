f84db819be006d82b24ea3e751079e0099f93ef0
keyan.namaghi <keyan.namaghi@sainsburys.co.uk>
2022-04-16 01:25:23 +0100

    :bug: fix disabled button on light theme

--- components/CreateRoom.jsx
+++ components/CreateRoom.jsx
@@ -23,7 +23,7 @@ export const CreateRoom = ({ toggleShow }) => {
           type="submit"
           value="Create"
           disabled={true}
-          className="w-full text-white bg-emerald-700 hover:bg-emerald-800 disabled:bg-slate-50 disabled:cursor-not-allowed disabled:opacity-25 focus:ring-4 focus:outline-none focus:ring-emerald-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-emerald-600 dark:hover:bg-emerald-700 dark:focus:ring-emerald-800"
+          className="w-full text-white bg-emerald-700 hover:bg-emerald-800 disabled:cursor-not-allowed disabled:opacity-25 focus:ring-4 focus:outline-none focus:ring-emerald-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-emerald-600 dark:hover:bg-emerald-700 dark:focus:ring-emerald-800"
         />
         <input
           type="button"
--- components/JoinRoom.jsx
+++ components/JoinRoom.jsx
@@ -24,7 +24,7 @@ export const JoinRoom = ({ toggleShow }) => {
           type="submit"
           value="Join"
           disabled={true}
-          className="w-full text-white bg-emerald-700 hover:bg-emerald-800 disabled:bg-slate-50 disabled:cursor-not-allowed disabled:opacity-25 focus:ring-4 focus:outline-none focus:ring-emerald-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-emerald-600 dark:hover:bg-emerald-700 dark:focus:ring-emerald-800"
+          className="w-full text-white bg-emerald-700 hover:bg-emerald-800 disabled:cursor-not-allowed disabled:opacity-25 focus:ring-4 focus:outline-none focus:ring-emerald-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-emerald-600 dark:hover:bg-emerald-700 dark:focus:ring-emerald-800"
           //   onClick={sendData}
         />
         <input
