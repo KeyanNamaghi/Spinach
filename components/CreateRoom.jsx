f5cb3c190e31a58fa8fc5b9c3de32f47ad6c2454
keyan.namaghi <keyan.namaghi@sainsburys.co.uk>
2022-04-16 01:25:54 +0100

    :coffin: removed unused import

--- components/CreateRoom.jsx
+++ components/CreateRoom.jsx
@@ -1,5 +1,3 @@
-import Image from 'next/image'
-
 export const CreateRoom = ({ toggleShow }) => {
   return (
     <div className="p-4 max-w-lg bg-white rounded-lg border border-gray-200 shadow-md sm:p-6 lg:p-8 dark:bg-gray-800 dark:border-gray-700 flex flex-col items-center">
