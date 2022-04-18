5d6ab3eaef9bfeb027b157bd527f4e970449bb03
keyan.namaghi <keyan.namaghi@sainsburys.co.uk>
2022-04-18 01:48:51 +0100

    :sparkles: create_room basics done for server

--- README.md
+++ README.md
@@ -18,16 +18,9 @@ else:
 
 inputs: name, password(optional)\*
 
-if room exists:
-
-- ⬜️ create room with name and password
-- ⬜️ add user to room with name
-- ⬜️ add event to confirm user joined room
-- ⬜️ add event to tell user the room code
-
-else:
-
-- ⬜️ nothing happens
-- ⬜️ add event to show connection failed
+- ✅ create room with name and password
+- ✅ add user to room with name
+- ✅ add event to confirm user joined room
+- ✅ add event to tell user the room code
 
 - password functionality to be added later
--- server/server.js
+++ server/server.js
@@ -20,26 +20,45 @@ const io = new Server(server, {
 io.on('connection', (socket) => {
   console.log(`User Connected: ${socket.id}`)
 
-  socket.on('join_room', ({ room }, callback) => {
-    // if (!rooms[room]) {
-    //   // Create a new room if it doesn't exist
-    //   rooms[room] = {
-    //     users: []
-    //   }
-    // }
-
+  socket.on('join_room', ({ name, room }, callback) => {
     if (!rooms[room]) {
       // message user telling them that the room doesn't exist
       callback({ connected: false, error: 'Room does not exist' })
     } else if (!rooms[room]?.users?.some((el) => el.id === socket.id)) {
       // add user to room if they don't exist in the room
       socket.join(room)
-      rooms[room].users.push({ id: socket.id, ready: false })
+      rooms[room].users.push({ id: socket.id, name, ready: false })
       callback({ connected: true, error: '' })
       console.log(`User ${socket.id} added to room: ${room}`)
     }
 
     console.log('Users in Room: ', rooms[room]?.users)
+  })
+
+  socket.on('create_room', ({ name, password }, callback) => {
+    // generate a code for the room (4 letters), could be improved
+    const generateCode = () => {
+      // generate 4 capital letter room code
+      const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+      const room = Array.from({ length: 4 }, () => letters[Math.floor(Math.random() * letters.length)]).join('')
+
+      if (rooms[room]) {
+        // lmao what are the chances?
+        return generateCode()
+      } else {
+        return room
+      }
+    }
+
+    // create room
+    const roomCode = generateCode()
+    rooms[roomCode] = {
+      users: [{ id: socket.id, name, ready: false }],
+      password
+    }
+
+    callback({ connected: true, error: '', room: roomCode })
+    console.log(`User ${socket.id} created room: ${room}`)
   })
 
   socket.on('send_message', (data) => {
