8db44cd2cd27d9376f0d244e3e989211440cfa99
keyan.namaghi <keyan.namaghi@sainsburys.co.uk>
2022-04-20 00:42:56 +0100

    click header to go back to homepage as a hack to make testing easier

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,10 @@
 import Head from 'next/head'
+import { useRouter } from 'next/router'
 import { ThemeToggle } from './ThemeToggle'
 
 export const Header = ({ theme, toggleTheme }) => {
+  const router = useRouter()
+
   return (
     <>
       <Head>
@@ -12,7 +15,9 @@ export const Header = ({ theme, toggleTheme }) => {
       <header className="flex items-center justify-between flex-wrap bg-gray-900 p-4">
         <div className="flex items-center flex-shrink-0 text-white mr-6">
           <ThemeToggle theme={theme} toggleTheme={toggleTheme} />
-          <span className="font-semibold text-xl tracking-tight">Spinach 🌱</span>
+          <span className="font-semibold text-xl tracking-tight" onClick={() => router.push('/')}>
+            Spinach 🌱
+          </span>
         </div>
       </header>
     </>
